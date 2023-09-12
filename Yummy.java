4f3487b793370fcdedea6b3c81fb1e95e20704a6
ddarrisoconnor <danieldarrisoconnor@billericak12.com>
2023-09-12 10:21:43 -0400

    yummy commit

---
+++ Yummy.java
@@ -0,0 +1,12 @@
+public class Yummy {
+    public static void main(String [] args){
+        System.out.println("Yummy");
+        chocolate();
+    }
+    public static void chocolate(){
+        System.out.print("Chocolate ");
+        System.out.println("Cake");
+
+    }
+    
+}
