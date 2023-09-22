0ee882cdf29a5d94b39de1f05a97ca9d5391eb7a
danieldarrisoconnor <danieldarrisoconnor@billericak12.com>
2023-09-22 10:12:41 -0400

    commit

--- MathEx.java
+++ MathEx.java
@@ -1,14 +1,35 @@
 public class MathEx {
     public static void main(String [] args){
-        swap(648);
+       System.out.println(swap(123));
+       System.out.println(swap(567));
+       System.out.println(swap(384));
+
+       timeLeft(1,15,4,36);
+       timeLeft(1,34,8,20);
+       timeLeft(10,2,14,6);
+       
+       System.out.println(dayOfWeek(5,15));
+       System.out.println(dayOfWeek(0,18));
+       System.out.println(dayOfWeek(1,24));
 
     }
         
-    public static void swap(int y){
+    public static int swap(int y){
         int ones = y%10;
         int tens = y%100/10;
-        System.out.println(ones);
-        System.out.println(tens);
-         
+        int newNumber = y/100*100 + ones * 10 + tens;
+        return newNumber;    
+    }
+    
+    public static void timeLeft(int curHour, int curMin, int depHour, int depMin){
+        int curTime = curHour * 60 + curMin;
+        int depTime = depHour * 60 + depMin;
+        int minLeft = depTime - curTime;
+        System.out.println(minLeft/60 + " hours and " + minLeft%60 + " minutes.");
+    }
+
+    public static int dayOfWeek(int startDay, int curDate){
+        int day = (curDate + startDay - 1) % 7;
+        return day;
     }
 }
