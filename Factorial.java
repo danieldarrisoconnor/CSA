e419484265236e9c371beb9712064cf3ed754f1c
danieldarrisoconnor <danieldarrisoconnor@billericak12.com>
2023-12-08 10:21:20 -0500

    big ddoc

---
+++ Factorial.java
@@ -0,0 +1,27 @@
+public class Factorial {
+
+    public static long calcFactorial(int factorial){
+        long product = 1;
+        for (int i = 1; i <= factorial; i ++){
+            product *= i;
+        }
+        return product;
+    }
+
+    public static double calcE(double e){
+        for (int i = 1,)
+        System.out.println(calcFactorial(i));
+
+
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i ++) {
+            System.out.println(calcFactorial(i));
+        }
+    }
+
+}
+
+    
